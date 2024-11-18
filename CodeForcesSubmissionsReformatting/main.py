b35239e27b3b79ff79cca5e192e5ba01cb2fc826
Saif Medhat <seifmedhatfarouk@gmail.com>
2024-11-18 17:46:24 +0200

    Add CodeForces Submissions Reformatting: Reformate the submissions fetched from codeforces  to adapt the comparing method

---
+++ CodeForcesSubmissionsReformatting/main.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+def rename_files():
+    submissions_folder = 'submisions'
+
+    with open('submissions.json') as f:
+        data = json.load(f)
+
+    for submission in data['result']:
+        id = submission['id']
+        verdict = submission['verdict']
+        authorHandle = submission['author']['members'][0]['handle']
+        problemIndex = submission['problem']['index']
+
+        if verdict == 'OK':
+            verdict = 'AC'
+
+        old_file = os.path.join(submissions_folder, f'{id}.cpp')
+        new_file = os.path.join(submissions_folder, f'{id}_{verdict}_{authorHandle}_{problemIndex}.cpp')
+
+        try:
+            print(f'Renamed: {old_file} -> {new_file}')
+
+            os.rename(old_file, new_file)
+            print(f'Renamed: {old_file} -> {new_file}')
+        except FileNotFoundError:
+            print(f'File not found: {old_file}')
+        except Exception as e:
+            print(f'Error renaming {old_file}: {e}')
+
+rename_files()
